f45b0681c7e93a8c6860dad71a82a0606af52c55
Phan Nguyen <nh314@outlook.com>
2020-10-29 22:47:56 +0700

    Task 4 & 5

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
 class Course extends Component {
+
     render () {
+        const [, title] = this.props.location.search.replace(/^\?/, "").split('=');
+
         return (
             <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+                <h1>{decodeURIComponent(title)}</h1>
+                <p>You selected the Course with ID: {this.props.match.params.id}</p>
             </div>
         );
     }
